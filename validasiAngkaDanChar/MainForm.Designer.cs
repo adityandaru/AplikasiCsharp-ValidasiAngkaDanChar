e51abd5700a3382f69307a1aba8a6c1bae501e24
ndaru <ndaru742@gmail.com>
2014-04-14 19:25:00 -0400

    first commit

---
+++ validasiAngkaDanChar/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: This
+ * Date: 11/6/2013
+ * Time: 8:59 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace validasiAngkaDanChar
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(24, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Number";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(24, 72);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Characters";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(93, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(195, 20);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(93, 69);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(195, 20);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2KeyPress);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(322, 114);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "validasiAngkaDanChar";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ validasiAngkaDanChar/MainForm.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: This
+ * Date: 11/6/2013
+ * Time: 8:59 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace validasiAngkaDanChar
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			this.MaximumSize = new Size(338, 152);
+			this.MinimumSize = new Size(338, 152);
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar)) {
+				e.Handled=true;
+			}
+		}
+		
+		void TextBox2KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar)) {
+				e.Handled=true;
+			}
+		}
+	}
+}
